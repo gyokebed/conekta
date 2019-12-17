dd7832b44bc552f5b8b9c4f58a97f5227ee7a514
Gustavo Yokebed <g.yokebed@gmail.com>
2019-12-17 13:47:37 -0600

    Added fake services

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { detail } from "./services/fakeDetailService";
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>Hello Conekta</div>
-        )
-    }
+class App extends React.Component {
+  render() {
+    return <div>Hello Conekta</div>;
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
