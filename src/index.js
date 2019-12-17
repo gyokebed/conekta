19f864517c6a2ee0adcd034c2bea5b9007f525c7
Gustavo Yokebed <g.yokebed@gmail.com>
2019-12-17 14:32:53 -0600

    Added state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { detail } from "./services/fakeDetailService";
+import { records } from "./services/fakeRecordsService";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      payments: records.data.payments
+    };
+  }
   render() {
-    return <div>Hello Conekta</div>;
+    return (
+      <div>
+        {this.state.payments.map(payment => {
+          return <h1>{payment.status}</h1>;
+        })}
+      </div>
+    );
   }
 }
 
