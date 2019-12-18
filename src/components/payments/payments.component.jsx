86b108462383d80343aef4fa210bd236c568bb58
Gustavo Yokebed <g.yokebed@gmail.com>
2019-12-17 18:36:15 -0600

    Added componentDidMount lifecyle

--- src/components/payments/payments.component.jsx
+++ src/components/payments/payments.component.jsx
@@ -5,12 +5,17 @@ class Payments extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      payments: records.data.payments
+      payments: []
     };
   }
+
+  componentDidMount() {
+    const payments = records.data.payments;
+    this.setState({ payments });
+  }
+
   render() {
     const { payments } = this.state;
-    console.log(payments);
     return payments.map(payment => {
       return <h1>{payment.customer.name}</h1>;
     });
