3c5ea4aefb1a00cda38b2eeb73e2c4e8a2603051
Gustavo Yokebed <g.yokebed@gmail.com>
2019-12-19 14:27:28 -0600

    Change props name on table component

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import TableBody from "./table-body.component.jsx";
 import TableHeader from "./table-header.component.jsx";
 
-const Table = ({ data: payments }) => {
+const Table = ({ data }) => {
   return (
     <table className="table">
       <TableHeader />
-      <TableBody data={payments} />
+      <TableBody data={data} />
     </table>
   );
 };
