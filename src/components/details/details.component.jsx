38b396ec98ee82192753598367b0600a435dd4c6
Gustavo Carrillo (Laptop) <g.yokebed@gmail.com>
2019-12-18 20:07:17 -0600

    Added Data to payment Details

--- src/components/details/details.component.jsx
+++ src/components/details/details.component.jsx
@@ -5,7 +5,11 @@ class Details extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      paymentDetails: {}
+      paymentDetails: {
+        customer: {},
+        line_items: [],
+        nested_charges: [{ payment_method: {} }]
+      }
     };
   }
 
@@ -16,14 +20,71 @@ class Details extends Component {
   }
 
   render() {
-    const { id } = this.props.match.params;
-    const { paymentDetails } = this.state;
+    const { id: paymentId } = this.props.match.params;
+    const {
+      paid_at,
+      customer: { name, email, phone },
+      nested_charges: [
+        {
+          failure_message,
+          amount,
+          created,
+          status,
+          payment_method: {
+            bank,
+            brand,
+            type,
+            last4,
+            name: nameOnCard,
+            exp_month,
+            exp_year,
+            auth_code
+          }
+        }
+      ]
+    } = this.state.paymentDetails;
+
+    console.log(name);
     return (
-      <h1>
-        {id}
-        <br></br>
-        {paymentDetails.amount}
-      </h1>
+      <div>
+        <div className="d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <div className="btn-group mr-2">
+            <button type="button" className="btn btn-sm btn-outline-secondary">
+              Data
+            </button>
+            <button type="button" className="btn btn-sm btn-outline-secondary">
+              Breakdown
+            </button>
+          </div>
+        </div>
+        <div>
+          <h4>PAYMENT STATUS</h4>
+          <div>Amount: ${amount}</div>
+          <div>Created At {created}</div>
+          <div>Paid At: {paid_at ? paid_at : "Not yet paid"}</div>
+          <div>{status}</div>
+          <div>{failure_message}</div>
+          <div>ORDER ID: {paymentId}</div>
+        </div>
+        <div>
+          <h4>CLIENT</h4>
+          <div>Name: {name}</div>
+          <div>Email: {email}</div>
+          <div>Phone: {phone}</div>
+        </div>
+        <div>
+          <h4>PAYMENT METHOD</h4>
+          <div>{bank}</div>
+          <div>{brand}</div>
+          <div>{type}</div>
+          <div>............{last4}</div>
+          <div>{nameOnCard}</div>
+          <div>
+            Expiration Date: {exp_month} / {exp_year}
+          </div>
+          <div>Authorization Code: {auth_code}</div>
+        </div>
+      </div>
     );
   }
 }
